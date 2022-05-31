49ac7eb82c46d611bdeb27ec0763e1bdda40c6dc
yubar46 <yubar4.6@gmail.com;git config --global user.email yubar4.6@gmail.com>
2022-05-31 08:45:49 +0430

    create a class Node Add T value in class Node Add Node<T>next in class Node create 4 constructor for class Node

---
+++ crc/Node.java
@@ -0,0 +1,24 @@
+public class Node<T> {
+
+    T value;
+    Node<T> next;
+
+    public Node(T value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    public Node(Node<T> next) {
+        this.next = next;
+        this.value= null;
+    }
+
+    public Node(T value, Node<T> next) {
+        this.next = next;
+        this.value=value;
+    }
+    public Node() {
+
+    }
+
+}
